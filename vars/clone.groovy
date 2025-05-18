ca54f88bd7ac78cf767f867d76174db022ec034c
keshav-raj-gautam <dilagautam@gmail.com>
2025-05-18 13:55:06 +0530

    Create clone.groovy

---
+++ vars/clone.groovy
@@ -0,0 +1,5 @@
+def call(){
+  echo "Cloning the repository"
+                git url: "https://github.com/keshav-raj-gautam/django-notes-app.git", branch: "main"
+                echo "Cloning complete"
+}
